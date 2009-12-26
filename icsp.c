cb6458aaff982f276119bcd4cbaf121d7e57d084
Richard Barlow <richard@richardbarlow.co.uk>
2009-12-26 16:22:41 +0000

    Add macros for controlling ICSP pins

--- icsp.c
+++ icsp.c
@@ -1,13 +1,23 @@
 #include "icsp.h"
 
-#ifndef _BV
-#define _BV(bit) (1 << (bit))
-#endif
-
 volatile uint8_t *icsp_OUTP;
 volatile uint8_t *icsp_INP;
 volatile uint8_t *icsp_DIRP;
 uint8_t icsp_CLKB, icsp_DATB;
+
+#ifndef _BV
+#define _BV(bit) (1 << (bit))
+#endif
+
+#define DATA_OUT  (*icsp_DIRP |=  _BV(icsp_DATB))
+#define DATA_IN   (*icsp_DIRP &= ~_BV(icsp_DATB))
+
+#define CLK_HIGH  (*icsp_OUTP |=  _BV(icsp_CLKB))
+#define CLK_LOW   (*icsp_OUTP &= ~_BV(icsp_CLKB))
+#define DATA_HIGH (*icsp_OUTP |=  _BV(icsp_DATB))
+#define DATA_LOW  (*icsp_OUTP &= ~_BV(icsp_DATB))
+
+#define DATA_VAL  (*icsp_INP & _BV(icsp_DATB))
 
 void icsp_init() {
 	/* ICSPCLK to output and ICSPDAT to input */
